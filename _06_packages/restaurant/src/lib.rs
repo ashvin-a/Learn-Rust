a63e2ee24b95f790d58c87cf743b2bd4fa8bb5a1
ashvin <ashvinanilkumarsh@gmail.com>
2025-02-06 07:33:49 +0530

    Add structs, implementations and enums for learning privacy rules.

--- _06_packages/restaurant/src/lib.rs
+++ _06_packages/restaurant/src/lib.rs
@@ -9,6 +9,24 @@ mod frontend {
 
         }
     }
+
+    pub struct Breakfast{
+        pub toast: String, // ! Everything in Rust is private by default.
+        seasonal_fruit: String
+    }
+
+    impl Breakfast{
+        pub fn summer(toast: &str)-> Breakfast{
+            Breakfast{
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches")
+            }
+        }
+    }
+    pub enum Appetizer{ //* If you mark an Enum public, the attributes will also be public
+        Soup,           //* This is not the case for implementations.
+        Salad
+    }
     mod serving{
         fn take_order(){
 
