39da8fd2be744a5be70ad904d6b861ff89e889f5
ashvin <ashvinanilkumarsh@gmail.com>
2025-02-06 07:36:31 +0530

    Learn about aliases.

--- _06_packages/src/main.rs
+++ _06_packages/src/main.rs
@@ -2,7 +2,15 @@ fn main() {
     //? Binary Crates and Library Crates
     println!("Hello, world!");
 
+    fn fun1() -> Result{
+        Ok(())
+    }
+
+    fn fun2() -> IOResult<()>{ //* Using aliases */
+        Ok(())
+    }
+
     // For creating library file via cargo
     //? cargo run --lib project-name
-    
+    eat_at_restaurant()
 }
