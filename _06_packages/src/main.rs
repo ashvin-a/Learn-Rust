51d90b6855529dc9d97d38a220d938125e37868e
ashvin <ashvinanilkumarsh@gmail.com>
2025-02-06 07:36:47 +0530

    Learn about clubbing imports

--- _06_packages/src/main.rs
+++ _06_packages/src/main.rs
@@ -1,3 +1,10 @@
+use restaurant::eat_at_restaurant;
+
+// use std::fmt::Result;
+// use std::io::Result as IOResult;
+
+use std::{fmt::Result, io::Result as IOResult}; // Same as above
+
 fn main() {
     //? Binary Crates and Library Crates
     println!("Hello, world!");
