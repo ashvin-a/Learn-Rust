884e4694ef75ed407aac0d8fee40210119f0a2b8
ashvin <ashvinanilkumarsh@gmail.com>
2025-03-16 21:11:58 +0530

    Add code to handle file reading.

--- _09_error_handling/src/main.rs
+++ _09_error_handling/src/main.rs
@@ -1,5 +1,34 @@
+use core::error;
+use std::{fmt::Error, fs::{File, read_to_string}, io::{ErrorKind, BufRead, BufReader}};
+
 fn main() {
-    a();
+    // a();
+    // How to handle error if it occurs
+    let f = File::open("hello.txt").unwrap_or_else(|error|{
+        if error.kind() == ErrorKind::NotFound{
+            File::create("hello.txt").unwrap_or_else( |error|{
+                panic!("Error in creating file! {:?}", error);
+            })
+        }
+        else{
+            panic!("Error in opening the file! {:?}", error)
+        }
+    }
+    );
+    // match read_to_string("hello.txt"){
+    //     Ok(contents) => println!("The file content : {:?}", contents),
+    //     Err(error)=> println!("An error occured : {:?}", error)
+    // }
+    //? OR we could do something like this
+    let reader = BufReader::new(f); //? By doing so we can use f.
+    println!("The file content :");
+    for line in reader.lines() {
+        match line {
+            Ok(content) => println!("{:?}", content),
+            Err(error) => println!("Error reading line: {:?}", error),
+        }
+    }
+
 }
 
 fn a(){
